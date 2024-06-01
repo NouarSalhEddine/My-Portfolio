05e8f06d5258aaced0c6ec9d4361427ee7b49b52
nouarsalheddine <nouarsalheddine@gmail.com>
2024-06-01 22:45:44 +0100

    sidebar

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -70,7 +70,7 @@ const Sidebar = () => {
             textTransform="uppercase"
             variants={fadeInUp}
           >
-            Marc Jhon
+            SalhEddine
           </MotionHeading>
           <MotionHeading
             as="h2"
@@ -82,7 +82,7 @@ const Sidebar = () => {
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
           >
-            Lawingco.
+            NOUAR.
           </MotionHeading>
           <MotionText
             colorScheme="gray"
@@ -90,7 +90,7 @@ const Sidebar = () => {
             className={styles.marginTopForce}
             variants={fadeInUp}
           >
-            Or you could call me KL. That works too . . .
+            Or you could call me Wingo. That works too . . .
           </MotionText>
 
           <MotionHeading
@@ -115,8 +115,8 @@ const Sidebar = () => {
               {' '}
               Thank you!
             </Text>
-            <br />I am software engineer that specializes at backend apis, front
-            end integration, recently found myself studying UX too.
+            <br />I am software engineer that specializes at front
+            end integration, backend apis, recently found myself studying information system too.
           </MotionText>
           <MotionButton
             size="lg"
@@ -128,7 +128,7 @@ const Sidebar = () => {
             width="120px"
             variants={simpleOpacity}
             as={'a'}
-            href="mailto:marcjhon18@gmail.com"
+            href="mailto:nouarsalheddine@gmail.com"
             target="_blank"
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
