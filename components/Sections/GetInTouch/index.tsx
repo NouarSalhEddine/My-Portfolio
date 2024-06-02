69de500f7d0fea22062dceef1f0bfdb18b5eb35d
nouarsalheddine <nouarsalheddine@gmail.com>
2024-06-02 23:54:29 +0100

    update footer

--- components/Sections/GetInTouch/index.tsx
+++ components/Sections/GetInTouch/index.tsx
@@ -57,7 +57,7 @@ const GetInTouch = () => {
         stuff, anything is cool. So feel free to message me on any of my social
         media or shoot me an{' '}
         <Link
-          href="mailto:marcjhon18@gmail.com"
+          href="mailto:nouarsalheddine@gmail.com"
           target="_blank"
           rel="noreferrer"
         >
@@ -77,14 +77,14 @@ const GetInTouch = () => {
           variant="description"
           textDecoration="none"
           rel="noreferrer"
-          href="https://github.com/klawingco/kl_portfolio"
+          href="https://github.com/nouarsalheddine"
           target="_blank"
           _focus={{ boxShadow: 'none' }}
         >
           <Text as="span">
             <Icon as={RiGithubFill} h={6} w={6} /> <br />
             Designed and Made with <Icon as={RiHeartPulseFill} /> <br />
-            KL Lawingco <Icon as={RiCopyleftLine} /> 2021
+            SALHEDDINE NOUAR <Icon as={RiCopyleftLine} /> 2021
           </Text>
         </Link>
       </Box>
