7b1e946a566a9d39a6e483f21c3aa6e39b76cd37
nouarsalheddine <nouarsalheddine@gmail.com>
2024-06-02 19:50:43 +0100

    update avatar

--- components/Avatar/index.tsx
+++ components/Avatar/index.tsx
@@ -11,8 +11,8 @@ import { useEffect } from 'react'
 import { avatarAnimation } from 'config/animations'
 
 const AvatarImages = {
-  DarkMode: '/KL_avatar.png',
-  LightMode: './KL_avatar_light.png',
+  DarkMode: './SN_avatar.png',
+  LightMode: './SN_avatar_light.png',
 }
 
 declare global {
